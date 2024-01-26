e006c89554e147d204c2312f99b409fab31c2aa1
Steffen Kroggel <kroggel@rkw.de>
2024-01-26 15:30:58 +0100

    [CHANGE] Compatibility with TYPO3 v10

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,23 +1,30 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-//=================================================================
-// Register Plugins
-//=================================================================
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'RKW.RkwEtracker',
-    'Pi1',
-    'RKW eTracker Tracklet'
-);
+call_user_func(
+    function($extKey)
+    {
+        //=================================================================
+        // Register Plugins
+        //=================================================================
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Pi1',
+            'RKW eTracker Tracklet'
+        );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'RKW.RkwEtracker',
-    'Pi2',
-    'RKW eTracker Redirect'
-);
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Pi2',
+            'RKW eTracker Redirect'
+        );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'RKW.RkwEtracker',
-    'Pi3',
-    'RKW eTracker OptOut'
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+            $extKey,
+            'Pi3',
+            'RKW eTracker OptOut'
+        );
+
+    },
+    'rkw_etracker'
 );
